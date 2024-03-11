a9c29fca94fa9e350f491183d46e654d10740446
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2024-03-11 20:56:00 -0300

    commit 3

---
+++ bkp.sh
@@ -0,0 +1,2 @@
+cp /c/Users/Otavio/Documents/cursos/alura_cursos/EscolaDevOps/CursosLinux/LinuxTerminal/linux_terminal/pasta_logs/*.txt /c/Users/Otavio/Documents/cursos/alura_cursos/EscolaDevOps/CursosLinux/LinuxTerminal/linux_terminal/backup
+
